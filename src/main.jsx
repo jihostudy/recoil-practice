fca24c13236787eea226cfe81c666bafa8215d2e
Ji__ho <cale.jiho@gmail.com>
2023-12-27 21:59:24 +0900

    #1.내가 사용할 전역 상태의 범위를 지정. <RecoilRoot> 보통 최상위 루트에 지정한다.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RecoilRoot } from "recoil";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* #1. 내가 사용할 전역 상태의 범위를 지정. <RecoilRoot> 보통 최상위 루트에 지정한다. */}
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
+);
